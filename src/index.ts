72347bb0a12d1884efad4cdd15dd198704951779
Himenon <k.himeno314@gmail.com>
2018-09-04 02:35:03 +0900

    Metricsが取れるようになった

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,23 @@
-export const hello = (name: string): string => {
-    return `Hello ${name}`;
+import { launch } from 'puppeteer'
+
+const main = async () => {
+  const url = ''
+  const MEASUREMENT_TIME = 10 // sec
+  let flag: boolean = true
+  const browser = await launch()
+  const page = await browser.newPage()
+  await page.goto(url)
+  await page.screenshot({ path: 'output/screenshot.png', fullPage: true })
+  const timerNumber = setInterval(async () => {
+    if (flag) {
+      console.log(await page.metrics())
+    }
+  }, 500)
+  setTimeout(async () => {
+    flag = false
+    clearInterval(timerNumber)
+    await browser.close()
+  }, MEASUREMENT_TIME * 1000)
 }
+
+main()
